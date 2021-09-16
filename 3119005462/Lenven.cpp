79e1727791f4441e5250020209e913265af1adc8
3119005462_黄有权 <2500334391@qq.com>
2021-09-16 17:32:39 +0800

    完成基本功能，第一次提交

---
+++ 3119005462/Lenven.cpp
@@ -0,0 +1,93 @@
+#include"Leven.h"
+
+Leven::Leven() {
+
+}
+Leven::~Leven() {
+
+	origFile.close();
+	copFile.close();
+	ansFile.close();
+
+}
+void Leven::init(int argc, char* argv[]) {
+
+	//打开文件
+	origFile.open(argv[1], ios::in);
+	copFile.open(argv[2], ios::in);
+	ansFile.open(argv[3], ios::out | ios::app);
+	if (!origFile.is_open() && !copFile.is_open() && !ansFile.is_open()) {
+		cout << "文件打开失败，请查看文件路径。" << endl;
+	}
+
+}
+void Leven::Run() {
+	SentenceToOne();
+	Value();
+	writeValue();
+}
+void Leven::writeValue() {
+	ansFile << setprecision(4) << sum * 100 << "%" << '\n' << endl;
+	cout << "结果成功写入文件!" << endl;
+}
+
+float Leven::getLD(string str1, string str2, int len1, int len2) {
+	int temp;
+	vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1));//len1行len2列的二维数组，记录状态值。
+	//初始化
+	for (int i = 1; i <= len1; i++) {  //例dp[2][0]表示一个长度为2的字符串str1与一个空字符串str2的最小编辑距离为2。
+		dp[i][0] = i;
+	}
+	for (int j = 1; j <= len2; j++) {
+		dp[0][j] = j;
+	}
+	for (int i = 1; i <= len1; i++) {
+		for (int j = 1; j <= len2; j++) {
+			if (str2[j - 1] == str1[i - 1])
+				dp[i][j] = dp[i - 1][j - 1];
+			else {
+				temp = min(dp[i][j - 1], dp[i - 1][j]);
+				dp[i][j] = min(temp, dp[i - 1][j - 1]) + 1;
+			}
+		}
+	}
+	return dp[len1][len2];
+}
+void Leven::Value() {
+	vector<float>::iterator iter;
+	for (iter = ansArray.begin(); iter != ansArray.end(); iter++) {
+		sum += *iter;
+	}
+	sum /= ansArray.size(); //查重率
+
+	cout << "文本查重率=" << setprecision(4) << sum * 100 << "%\n";
+}
+
+
+
+void Leven::SentenceToOne() {
+	clock_t start, end;//记录程序运行时间
+	start = clock();
+	while (!origFile.eof() || !copFile.eof()) {
+
+		int sen1 = 0, sen2 = 0;
+
+		getline(origFile, str1);
+		getline(copFile, str2);
+
+		if (str1.empty() || str2.empty()) continue;
+
+
+		int len1, len2;
+		float ld;
+		len1 = str1.size();
+		len2 = str2.size();
+
+		ld = getLD(str1, str2, len1, len2);
+		ans = 1.0 - 1.0 * ld / max(len1, len2);
+
+		ansArray.push_back(ans);
+	}
+	end = clock();
+	cout << "运行时间：" << 1.0 * (0LL + end - start) / CLOCKS_PER_SEC << "s\n";
+}
---
+++ 3119005462/main.cpp
@@ -0,0 +1,29 @@
+﻿#include"main.h"
+
+using namespace std;
+
+
+
+bool check(int argc, char* argv[]) {  //检查命令函参数
+	if (argc != 4) {
+		cout << "ERROR:参数错误！\n";
+		return false;
+	}
+	if (_access(argv[1], 00) == -1 || _access(argv[2], 00) == -1) {
+		if (_access(argv[1], 00) == -1) cout << "Refer Adress Error\n";
+		if (_access(argv[2], 00) == -1) cout << "Test Adress Error\n";
+		return false;
+	}
+	return true;
+}
+
+int main(int argc,char* argv[]) {
+
+	if (check(argc, argv)) {
+		Leven leven;
+		leven.init(argc,argv);
+		leven.Run();
+	}
+	system("pause");
+	return 0;
+}
